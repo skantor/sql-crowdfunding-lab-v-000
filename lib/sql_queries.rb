0e6de4ee35d5ffe23f19dcfe3e39ae58c9d7258a
Sara Kantor <sara.kantor.nv@gmail.com>
2020-10-05 22:09:06 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
  (sum(pledges.amount) - projects.funding_goal) profit
  FROM projects JOIN pledges 
  ON projects.id = pledges.project_id 
- HAVING total_pledge >= projects.funding_goal 
+ HAVING profit >= 0 
  GROUP BY projects.title"
 end
 
